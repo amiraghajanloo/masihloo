2e6846c7d0ac61d705882612d7ee1f4888041c1e
amiraghajanloo <49584156+amiraghajanloo@users.noreply.github.com>
2024-07-22 01:22:48 +0330

    Add files via upload

---
+++ baseInfo.php
@@ -0,0 +1,9 @@
+<?php
+error_reporting(0);
+$botToken = '6098152957:AAF4a-3sEvYktC628aTn-oZDEKlJNrvorYs';
+$dbUserName = 'hfEl9DvFdQ33w';
+$dbPassword = 'OBzDeaEkT4fviw';
+$dbName = 'masihloo';
+$botUrl = 'https://bot.masihloo.click/masihloo/';
+$admin = 5888511088;
+?>
